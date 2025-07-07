fee51b0cee9e4fc9a3a8276ffe01b837d4f5c7a4
whals <whalsdud0131@gmail.cm>
2025-07-07 18:36:40 +0900

    DB connection

--- src/main/java/com/my131/edu_system/EduSystemApplication.java
+++ src/main/java/com/my131/edu_system/EduSystemApplication.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EduSystemApplication {
 
 	public static void main(String[] args) {
+		System.out.println("DB_HOST=" + System.getenv("DB_HOST"));
+		System.out.println("DB_PORT=" + System.getenv("DB_PORT"));
+		System.out.println("DB_NAME=" + System.getenv("DB_NAME"));
+		System.out.println("DB_USERNAME=" + System.getenv("DB_USERNAME"));
+		System.out.println("DB_PASSWORD=" + System.getenv("DB_PASSWORD"));
+
 		SpringApplication.run(EduSystemApplication.class, args);
 	}
-
 }
